45d6a5d7dedd4018d609c8dd41401c0c9d8becc4
Tripp Collins <tripp@TrippMac.local>
2021-04-08 14:52:33 -0400

    removed dependancies

--- bubbleMethod.m
+++ bubbleMethod.m
@@ -1,4 +1,4 @@
-function [LONobs LATobs TIMEobs HSobsMean HSobsStd HSobsNoSam] = bubbleMethod(obsLon, obsLat, obsTime, obsHs, gridLon,gridLat,gridTime,maxDistance,maxTimeDiff,gridStatus,minNumberObs) 
+function [LONobs LATobs TIMEobs HSobsMean HSobsStd HSobsNoSam WINDobsMean WINDobsStd WINDobsNoSam] = bubbleMethod(obsLon, obsLat, obsTime, obsHs, obsWind, gridLon,gridLat,gridTime,maxDistance,maxTimeDiff,gridStatus,minNumberObs) 
 % [LONobs LATobs TIMEobs HSobs] = bubbleMethod(obsLon, obsLat, obsTime, obsHs, gridLon,gridLat,gridTime,maxDistance,maxTimeDiff,gridStatus,minNumberObs) 
 %
 %troubleshooting
@@ -6,8 +6,7 @@ function [LONobs LATobs TIMEobs HSobsMean HSobsStd HSobsNoSam] = bubbleMethod(ob
 % obsHs = HSobsNA; gridLon = mdTest.lon; gridLat = mdTest.lat;
 % gridTime = mdTest.time; gridStatus = mdTest.gridstatus;
 % 
-%
-%
+
 %TO DO: put in a nearest option so it grabs just one altimeter sample
 %instead of an average
 
@@ -15,7 +14,7 @@ if sum([ismember(gridLon,359.5); ismember(gridLon,0)]) == 2
     for i = 1:length(gridLon)
         if gridLon(i) > 180
             gridLon(i) = gridLon(i) - 360;
-        end       
+        end         
     end
     for i = 1:length(obsLon)
         if obsLon(i) > 180
@@ -29,22 +28,26 @@ mdLatLength = length(gridLat);
 
 skipTime = ceil(maxTimeDiff/(gridTime(2)-gridTime(1)));
 mdTimeLength = length(gridTime(1:skipTime:end));
-
+% allocate
 meanHsByDistance = NaN(mdLonLength,mdLatLength,mdTimeLength);
 stdHsByDistance = NaN(mdLonLength,mdLatLength,mdTimeLength);
 noSamHsByDistance = NaN(mdLonLength,mdLatLength,mdTimeLength);
+meanWindByDistance = NaN(mdLonLength,mdLatLength,mdTimeLength);
+stdWindByDistance = NaN(mdLonLength,mdLatLength,mdTimeLength);
+noSamWindByDistance = NaN(mdLonLength,mdLatLength,mdTimeLength);
 % tic
 for i = 1:mdLonLength
     for j = 1:mdLatLength
         if gridStatus(i,j)==1
             for k = 1:skipTime:mdTimeLength
-                [xdummy,ydummy,distance] = latlon2xy(obsLat,obsLon,gridLat(j),gridLon(i));
-                [meanIndex dum2] = find(distance<=maxDistance &...
+                distance = latlon2dist(obsLat,obsLon,gridLat(j),gridLon(i));
+%                [xdummy,ydummy,distance] = latlon2xy(obsLat,obsLon,gridLat(j),gridLon(i));
+                [meanIndex, ~] = find(distance<=maxDistance &...
                     abs(gridTime(k)- obsTime) <= maxTimeDiff);                
                 if length(meanIndex) >= minNumberObs
                     currentData = obsHs(meanIndex);
-                    meanHsByDistance(i,j,k) = nanmean(currentData);
-                    stdHsByDistance(i,j,k) = nanstd(currentData);
+                    meanHsByDistance(i,j,k) = mean(currentData,'omitnan');
+                    stdHsByDistance(i,j,k) = std(currentData,'omitnan');
                     noSamHsByDistance(i,j,k) = length(currentData);                                        
                 end
             end
@@ -55,12 +58,18 @@ end
 meanHsByDistance = meanHsByDistance(:);
 stdHsByDistance  = stdHsByDistance(:);
 noSamHsByDistance = noSamHsByDistance(:);
+meanWindByDistance = meanWindByDistance(:);
+stdWindByDistance  = stdWindByDistance(:);
+noSamWindByDistance = noSamWindByDistance(:);
 
 indNaN = ~isnan(meanHsByDistance);
 
 HSobsMean  = meanHsByDistance(indNaN);
 HSobsStd   = stdHsByDistance(indNaN);
 HSobsNoSam = noSamHsByDistance(indNaN);
+WINDobsMean  = meanWindByDistance(indNaN);
+WINDobsStd   = stdWindByDistance(indNaN);
+WINDobsNoSam = noSamWindByDistance(indNaN);
 
 [LON, LAT, TIME] = ndgrid(gridLon,gridLat,gridTime(1:skipTime:end));
 LON  = LON(:);
